f763efa976a68ce7a75d0879f3313198a5136595
Laura-fr <laurafraysse84@gmail.com>
2021-01-05 18:29:33 +0100

    json updated

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Emoji = (props) => {
+  const [hoverLine, setHoverLine] = useState(false);
+
   return (
-    <div className="line">
+    <div
+      className="line"
+      onMouseEnter={() => setHoverLine(true)}
+      onMouseLeave={() => setHoverLine(false)}
+      onClick={() => {
+        navigator.clipboard.writeText(props.symbol);
+      }}
+    >
       <span>
         {props.title} {props.symbol}
       </span>
+      {hoverLine && <span className="copy">Click to copy !</span>}
     </div>
   );
 };
